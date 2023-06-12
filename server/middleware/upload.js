7a34090471974f9b29766b03f0ac18dc3667c96e
oussema <oghawi220@gmail.com>
2023-06-12 22:55:21 +0100

    upload2

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configure multer storage
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'upload/');
+		cb(null, './public/images/');
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname);
