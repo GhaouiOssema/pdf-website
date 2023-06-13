31e478554665427615a372de69d037bc14d4177c
oussema <oghawi220@gmail.com>
2023-06-13 01:14:54 +0100

    solve error 08

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -1,12 +1,22 @@
-const path = require('path');
+const PDF = require('../models/PDF');
 
 module.exports = {
 	async pdfView(req, res) {
-		{
-			const filename = req.params.filename;
-			const filePath = path.join(__dirname, 'uploads', filename);
+		const filename = req.params.filename;
 
+		try {
+			// Find the PDF document with the matching filename in the database
+			const pdfDocument = await PDF.findOne({ filename });
+
+			if (!pdfDocument) {
+				return res.status(404).send('PDF not found');
+			}
+
+			const filePath = pdfDocument.filepath;
 			res.sendFile(filePath);
+		} catch (error) {
+			console.error('Error retrieving PDF:', error);
+			res.status(500).send('Internal Server Error');
 		}
 	},
 };
