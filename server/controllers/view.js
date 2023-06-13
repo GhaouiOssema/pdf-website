35186c40f436315f77880e5df4a1e5cf72d2b323
oussema <oghawi220@gmail.com>
2023-06-13 14:55:34 +0100

    solve error 24

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -3,26 +3,11 @@ const path = require('path');
 
 module.exports = {
 	async pdfView(req, res) {
-		const filename = req.params.filename;
+		// Retrieve the PDF file based on the provided ID
+		const pdfId = req.params.id;
+		const filePath = path.join(__dirname, '../upload/', `${pdfId}.pdf`);
 
-		try {
-			// Retrieve the file path from the database based on the filename
-			const pdfDocument = await PDF.findOne({ filename });
-
-			if (!pdfDocument) {
-				return res.status(404).send('PDF not found');
-			}
-
-			const filePath = path.resolve(
-				__dirname,
-				'..',
-				'uploads',
-				pdfDocument.filepath
-			);
-			res.sendFile(filePath);
-		} catch (error) {
-			console.error('Error retrieving PDF:', error);
-			res.status(500).send('Internal Server Error');
-		}
+		// Send the PDF file to the client
+		res.sendFile(filePath);
 	},
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,6 @@ router.get('/pdfs', controllers.allPdfs.allPdfs);
 router.delete('/pdfs/:id', controllers.deletePdf.delete);
 router.get('/pdf/:id', controllers.getPdfById.onePdf);
 router.post('/upload', upload.single('pdf'), controllers.upload.uploadPdf);
-// router.get('/pdf/view/:filename', controllers.view.pdfView);
+router.get('/pdf/view/:filename', controllers.view.pdfView);
 
 module.exports = router;
