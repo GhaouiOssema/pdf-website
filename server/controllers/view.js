9d3cf53243c57059bf6f75b7a7f1908bad7b34b0
oussema <oghawi220@gmail.com>
2023-06-13 01:19:05 +0100

    solve error 09

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -1,18 +1,23 @@
 const PDF = require('../models/PDF');
+const path = require('path');
 
 module.exports = {
 	async pdfView(req, res) {
 		const filename = req.params.filename;
 
 		try {
-			// Find the PDF document with the matching filename in the database
+			// Retrieve the file path from the database based on the filename
 			const pdfDocument = await PDF.findOne({ filename });
 
 			if (!pdfDocument) {
 				return res.status(404).send('PDF not found');
 			}
 
-			const filePath = pdfDocument.filepath;
+			const filePath = path.join(
+				__dirname,
+				'uploads',
+				pdfDocument.filepath
+			);
 			res.sendFile(filePath);
 		} catch (error) {
 			console.error('Error retrieving PDF:', error);
