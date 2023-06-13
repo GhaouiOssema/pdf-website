1c3ba73d33f17ddd17c4182aa3dbbad7eed84125
oussema <oghawi220@gmail.com>
2023-06-13 01:07:08 +0100

    solve error 07

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
 	async pdfView(req, res) {
-		const filename = req.params.path;
+		{
+			const filename = req.params.filename;
+			const filePath = path.join(__dirname, 'uploads', filename);
 
-		const filePath = path.join(__dirname, 'uploads', filename);
-
-		res.sendFile(filePath);
+			res.sendFile(filePath);
+		}
 	},
 };
