e3854e3a9039c42c33c43dd9c27340815875385c
oussema <oghawi220@gmail.com>
2023-06-13 15:10:22 +0100

    solve error 25

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -5,7 +5,7 @@ module.exports = {
 	async pdfView(req, res) {
 		// Retrieve the PDF file based on the provided ID
 		const pdfId = req.params.id;
-		const filePath = path.join(__dirname, '../upload/', `${pdfId}.pdf`);
+		const filePath = path.join(__dirname, '../uploads/', `${pdfId}.pdf`);
 
 		// Send the PDF file to the client
 		res.sendFile(filePath);
--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configure multer storage
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, '../upload/');
+		cb(null, '../uploads/');
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname);
