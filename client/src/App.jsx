c3ef4c0d9d74b8acec8457ef9e3e842b801e5c67
oussema <oghawi220@gmail.com>
2023-06-21 00:38:54 +0200

    mvp_version_13

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,18 +8,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import PdfView from "./components/PdfView";
 
+import backgroundImg from "./assets/bg.png";
+
 const App = () => {
   return (
-      <div className="mx-auto flex flex-col flex-wrap">
-		<Navbar />
-        <div className="container mt-10">
-          <Routes>
-            <Route path="/" element={<FormSend />} />
-            <Route path="/pdf" element={<PdfFile />} />
-            <Route path="pdf/:id" element={<PdfDetails />} />
-            <Route path="/pdf/view/:id" element={<PdfView />} />
-          </Routes>
-        </div>
+    <div className="mx-auto flex flex-col flex-wrap">
+      <Navbar />
+      <div className="container mt-10">
+        <Routes>
+          <Route path="/" element={<FormSend />} />
+          <Route path="/pdf" element={<PdfFile />} />
+          <Route path="pdf/:id" element={<PdfDetails />} />
+          <Route path="/pdf/view/:id" element={<PdfView />} />
+        </Routes>
       </div>
     </div>
   );
