e531816768dde96fd0b22e40edafb0ea290c487f
unknown <oghawi220@gmail.com>
2023-08-21 01:26:14 +0200

    responsive design : home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -345,7 +345,7 @@ const Home = () => {
         </Tabs>
       </section>
 
-      <section className="dark:bg-gray-900 boor ">
+      <section className="dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-sm text-center">
             <h2 className="mb-4 text-4xl font-extrabold leading-tight text-gray-900 dark:text-white">
