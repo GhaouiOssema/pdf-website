3abbb9a522087a4325f2d35579c8faf4b7a1443b
unknown <oghawi220@gmail.com>
2023-08-22 19:16:52 +0200

    responsive design : home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import MobileView from "./MobileView";
 import DesktopView from "./DescktopView";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const Home = () => {
+  const [isNavbarVisible, setIsNavbarVisible] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setIsNavbarVisible(true);
+    } else {
+      setIsNavbarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="bg-gray-100">
       <div className="xl:block lg:block md:block hidden ">
@@ -19,14 +37,17 @@ const Home = () => {
       <div className="lg:hidden md:hidden sm:block xs:block">
         <MobileView />
       </div>
-      <div className="fixed bottom-6 right-6 z-50">
-        <button
-          onClick={scrollToTop}
-          className="bg-[#F0854A] text-white py-2 px-4 rounded-full shadow-md focus:outline-none focus:bg-[#F0854A] hover:bg-[#F0854A]"
-        >
-          <KeyboardArrowUpIcon size={24} />
-        </button>
-      </div>
+
+      {isNavbarVisible && (
+        <div className="fixed bottom-6 right-6 z-50">
+          <button
+            onClick={scrollToTop}
+            className="bg-[#F0854A] text-white py-2 px-4 rounded-full shadow-md focus:outline-none focus:bg-[#F0854A] hover:bg-[#F0854A]"
+          >
+            <KeyboardArrowUpIcon size={24} />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
